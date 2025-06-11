7066c8cf65943ee859a302999bff4b8eff2df363
L K R N <artemabramcuk45@gmail.com>
2025-06-11 18:36:39 +0300

    chore: настроил базовую конфигурацию

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+// app.use('/api', routes);
+
+export default app;
---
+++ src/routes/index.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+
+const router = Router();
+
+// TODO: подключить маршруты
+
+export default router;
